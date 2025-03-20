691cb1ff21cf9642eddee8bdf9331f727296a6de
Aaron Gibson <aaron@goreact.com>
2025-03-20 16:12:22 -0600

    adding migration order

--- database/migrations/2025_03_16_200526_create_categories_table.php
+++ database/migrations/2025_03_16_200526_create_categories_table.php
@@ -11,13 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('organizations', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('slug')->unique();
-            $table->foreignId('owner_id')->constrained('users');
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
@@ -26,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('organizations');
+        Schema::dropIfExists('categories');
     }
 };
--- database/migrations/2025_03_16_200530_add_card_id_to_transactions_table.php
+++ database/migrations/2025_03_16_200530_add_card_id_to_transactions_table.php
@@ -11,13 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('organizations', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('slug')->unique();
-            $table->foreignId('owner_id')->constrained('users');
-            $table->timestamps();
-            $table->softDeletes();
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->foreignId('card_id')->nullable()->constrained()->onDelete('set null');
         });
     }
 
@@ -26,6 +21,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('organizations');
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->dropForeign(['card_id']);
+            $table->dropColumn('card_id');
+        });
     }
 };
